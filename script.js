191afa988cd6a791ec0faeca722ff30f05d5e0dd
Tunahan Öztürk <tnhn17@windowslive.com>
2023-06-11 22:06:01 +0300

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+const accessKey = "wJmVYmceybi_-isAWGgQzsKlKsvCTyxJWa5GvhuUmNc" // unsplach API key
+
+const formEl = document.querySelector("form") // form kısmını değişkene atadık
+const imputEl = document.getElementById("search-input") // input kısmındaki veriyi bu değişkene atayacağız
+const searchResults = document.querySelector(".search-resukts") // Not: sınıf kullanıyorsak başına . yazmamız lazım querySelector ile.
+const showMore = document.getElementById("show-more-button") // ID kullandığımız için başına nokta koymaya gerek yok.
